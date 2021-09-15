8338c2e4f4a1360f64591f1ec0f231a69a547447
manfredbjorlin <manfred.bjorlin@gmail.com>
2021-09-15 14:57:53 +0200

    Removed error

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Unhealthy";
+            Status = "Healthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
