6f547a66f33cae51829b655319fe68961afeedfa
manfredbjorlin <manfred.bjorlin@gmail.com>
2021-09-15 14:54:14 +0200

    Introduced an error

--- apis/poi/web/Models/Healthcheck.cs
+++ apis/poi/web/Models/Healthcheck.cs
@@ -8,7 +8,7 @@ namespace poi.Models
         public Healthcheck()
         {
             Message = "POI Service Healthcheck";
-            Status = "Healthy";
+            Status = "Unhealthy";
         }
         [Newtonsoft.Json.JsonProperty(PropertyName = "message")]
         public string Message {get;set;}
